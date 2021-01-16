eb1ad2be813d64c30d6f38a21ad0171f9064427c
Gimmy1897 <gimmypapi@gmail.com>
2021-01-16 20:10:15 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+
+void main() => runApp(MaterialApp(home: BottomNavBar()));
+
+class BottomNavBar extends StatefulWidget {
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _page = 0;
+  GlobalKey _bottomNavigationKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        bottomNavigationBar: CurvedNavigationBar(
+          key: _bottomNavigationKey,
+          index: 1,
+          height: 50.0,
+          items: <Widget>[
+            Icon(Icons.add, size: 35, color: Colors.white),
+            Icon(Icons.list, size: 35, color: Colors.white),
+            Icon(Icons.compare_arrows, size: 35, color: Colors.white),
+            Icon(Icons.call_split, size: 35, color: Colors.white),
+          ],
+          color: Colors.cyan,
+          buttonBackgroundColor: Colors.cyan,
+          backgroundColor: Colors.white,
+          animationCurve: Curves.easeInOut,
+          animationDuration: Duration(milliseconds: 450),
+          onTap: (index) {
+            setState(() {
+              _page = index;
+            });
+          },
+          letIndexChange: (index) => true,
+        ),
+        body: Container(
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Text(_page.toString(), textScaleFactor: 10.0),
+                ElevatedButton(
+                  child: Text('Go To Page of index 2'),
+                  onPressed: () {
+                    final CurvedNavigationBarState navBarState =
+                        _bottomNavigationKey.currentState;
+                    navBarState.setPage(1);
+                  },
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
---
+++ lib/res/res.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class Color {}
+
+class Theme {}
